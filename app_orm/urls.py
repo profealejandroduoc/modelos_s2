df92317b99bd51fd8986fefbc6b27db23ba11899
El profe <al.sepulvedam@profesor.duoc.cl>
2024-06-04 15:52:38 -0400

    Lista y detalles

--- app_orm/urls.py
+++ app_orm/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, personas, detallepersona
 
 urlpatterns = [
-    path('', index, name='index')
+    path('', index, name='index'),
+    path('personas/',personas,name='personas'),
+    path('detallepersona/<id>',detallepersona,name='detallepersona')
 ]
--- app_orm/views.py
+++ app_orm/views.py
@@ -1,5 +1,39 @@
 from django.shortcuts import render
-
+from datetime import datetime, date
+from .models import Persona
+from django.shortcuts import get_object_or_404
 # Create your views here.
 def index(request):
-    return render(request,'app_orm/index.html')
+    hoy=datetime.now()
+    texto="Este es un texto desde la vista index"
+    asistentes=6
+    lista=[1,2,3,4,5,7]
+
+    datos={
+        "fecha":hoy,
+        "texto":texto,
+        "asistentes":asistentes,
+        "lista":lista
+    }
+
+
+    return render(request,'app_orm/index.html', datos)
+
+def personas(request):
+    personas=Persona.objects.all()
+
+    datos={
+        "personas":personas
+    }
+
+    return render(request,'app_orm/personas.html',datos)
+
+def detallepersona(request,id):
+    persona=get_object_or_404(Persona,rut=id)
+    #print(persona)
+    datos={
+
+        "persona":persona
+    }
+
+    return render(request,'app_orm/detallepersona.html', datos)
